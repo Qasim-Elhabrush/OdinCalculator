62197f3cf552a2c5a31a24bc28ed5b0e1d9cd3ce
Qasim-Elhabrush <qasimelhabrush@gmail.com>
2022-05-23 16:45:10 -0400

    post "=" fixed, limit characters in display

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 entry = "";
 //displays the number/symbol pressed in top box
 const writeEntry = function(value) {
-        entry += value;
-        document.getElementById("textEntry").innerText = entry;
-
+        if (entry.length < 20) {
+            entry += value;
+            document.getElementById("textEntry").innerText = entry;
+        } else {}
     }
     //calculates what is entered in top box
 const calculate = function() {
-        entry.replace(/[^-()\d/*+.]/g, '');
-        entry = eval(entry)
-        document.getElementById("textEntry").innerText = entry;
+        let calculatedEntry = eval(entry)
+        document.getElementById("textEntry").innerText = calculatedEntry;
+        entry = `${calculatedEntry}`;
     }
     //reloads page
 const clear = function() {
-        window.location.reload();
+        document.getElementById("textEntry").innerText = "";
+        entry = "";
     }
     //creates event listeners for each button
 for (let i = 0; i <= 9; i++) {
