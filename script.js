1ca6bf9e49b3657921cb6e297af84764c94b809b
Qasim-Elhabrush <qasimelhabrush@gmail.com>
2022-05-22 23:52:33 -0400

    CSS changes. Calculator complete and functional

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 entry = "";
+//displays the number/symbol pressed in top box
 const writeEntry = function(value) {
-    entry += value;
-    document.getElementById("textEntry").innerText = entry;
+        entry += value;
+        document.getElementById("textEntry").innerText = entry;
 
-}
+    }
+    //calculates what is entered in top box
 const calculate = function() {
-    entry.replace(/[^-()\d/*+.]/g, '');
-    alert(eval(entry));
-}
+        entry.replace(/[^-()\d/*+.]/g, '');
+        entry = eval(entry)
+        document.getElementById("textEntry").innerText = entry;
+    }
+    //reloads page
 const clear = function() {
-    window.location.reload();
+        window.location.reload();
+    }
+    //creates event listeners for each button
+for (let i = 0; i <= 9; i++) {
+    document.getElementById(`${i}`).addEventListener("click", function() { writeEntry(i); });
 }
-document.getElementById("0").addEventListener("click", function() { writeEntry(0); });
-document.getElementById("1").addEventListener("click", function() { writeEntry(1); });
-document.getElementById("2").addEventListener("click", function() { writeEntry(2); });
-document.getElementById("3").addEventListener("click", function() { writeEntry(3); });
-document.getElementById("4").addEventListener("click", function() { writeEntry(4); });
-document.getElementById("5").addEventListener("click", function() { writeEntry(5); });
-document.getElementById("6").addEventListener("click", function() { writeEntry(6); });
-document.getElementById("7").addEventListener("click", function() { writeEntry(7); });
-document.getElementById("8").addEventListener("click", function() { writeEntry(8); });
-document.getElementById("9").addEventListener("click", function() { writeEntry(9); });
 document.getElementById(".").addEventListener("click", function() { writeEntry("."); });
 document.getElementById("*").addEventListener("click", function() { writeEntry('*'); });
 document.getElementById("/").addEventListener("click", function() { writeEntry('/'); });
